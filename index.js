9bf739b75f40869bf2fa899fb1c07f7301e4558a
ruka-Design <2002rukahosomi@gmail.com>
2021-10-07 00:12:51 +0900

    画面TOPの座標を取得しました

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-document.getElementById("section_01").onclick = function () {
+// フェードインさせる要素を取得してくる
+const sections = document.getElementById("section_01");
+
+// 取得されたかの確認、クリックされた時背景色が変わる
+sections.onclick = function () {
   console.log("クリックされました");
+  sections.style.background = "tomato";
+};
+
+//画面TOPの座標を取得する
+window.onscroll = function (event) {
+  console.log(event.target.documentElement.scrollTop);
+
+  // sections.style.background = "tomato";
 };
