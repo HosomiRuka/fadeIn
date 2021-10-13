8c821842f5f98b25fcd7e74f81ca2c571bd60c3b
ruka-Design <2002rukahosomi@gmail.com>
2021-10-14 01:04:41 +0900

    一個ずつ作成中

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function animation() {
   // 動作確認テスト用
   // console.log(sections + "です");
   // console.log(topheight + "です");
-  // console.log(scrollTop + "です");
+  console.log(scrollTop + "です");
   // console.log(target + "です");
   // console.log(windowHeight + "です");
   // console.log(point + "です");
@@ -26,6 +26,7 @@ function animation() {
   // フェードインアニメーション
   // 条件
   if (scrollTop + 200 > target - windowHeight) {
+    console.log(target - windowHeight);
     // 透過変更
     sections.style.opacity = "1";
     // 座標変更
@@ -46,15 +47,24 @@ window.addEventListener("scroll", function () {
 });
 ///////////////////--------お手本ここまで-------///////////////////////
 
-let section_02 = document.getElementById("section_02");
-console.log(section_02);
-
-//マウスcursorのcursorの座標を取得する
+//マウスカーソルの座標を取得する
 window.addEventListener("mousemove", function (mouse) {
   //これがなんでmouseの役割果たせてるのか意味がわからん↑
   let mX = mouse.pageX;
   let mY = mouse.pageY;
 
-  console.log(mX);
-  console.log(mY);
+  // console.log(mX);
+  // console.log(mY);
 });
+
+//お手本を分解して一個ずつ作っていく
+//作りたいもの = 要素が指定の箇所まで来たときcssを追加する
+//指定の箇所 = どうわからせる？
+//1.対象となる要素を取得してくる
+let section_02 = document.getElementById("section_02");
+//要素の寸法と位置を取得
+let size = section_02.getBoundingClientRect();
+
+//動いてるか確認
+// console.log(section_02);
+// console.log(size);
