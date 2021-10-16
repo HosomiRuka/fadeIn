30abd9b66b14f4af3e8fb541abef08576de205d9
ruka-Design <2002rukahosomi@gmail.com>
2021-10-17 00:06:05 +0900

    if文復習

---
+++ ronriwa/index.js
@@ -0,0 +1,34 @@
+// A && B は、「AとBが両方成立する必要がある」（AND演算）
+// A || B は、「AまたはBのどちらかが成立すればOK」（OR演算）
+// !A は、「A以外なら何でもOK」（NOT演算）
+
+let textBox = document.getElementById("text");
+let btn = document.getElementById("btn");
+let h1 = document.getElementById("title");
+
+btn.onclick = function () {
+  let text = textBox.value;
+  // switch (text) {
+  //   case ("赤", "あか"):
+  //     h1.innerHTML = "止まれ";
+  //     break;
+  //   case ("青", "あお"):
+  //     h1.innerHTML = "GO!";
+  //     break;
+  //   case ("黄色", "きいろ"):
+  //     h1.innerHTML = "一時停止";
+  //     break;
+  // }
+  if (text > 5) {
+    h1.innerHTML = "5より大きい";
+  } else if (text == 5) {
+    h1.innerHTML = "5です！";
+  } else if (text < 5) {
+    h1.innerHTML = "5以下！";
+  } else {
+    h1.innerHTML = "入力内容が正しくありません";
+  }
+};
+console.log(textBox.value);
+console.log(btn);
+console.log(h1);
