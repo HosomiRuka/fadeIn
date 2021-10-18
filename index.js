890442949c4907400a2d4c779b6d12340e73e560
ruka-Design <2002rukahosomi@gmail.com>
2021-10-18 20:37:45 +0900

    30%理解,

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function animation() {
   // 要素の寸法と位置を取得,Element.getBoundingClientRect() メソッドは、要素の寸法と、そのビューポートに対する位置を返します。
   let topheight = sections.getBoundingClientRect();
   // 現在のスクロール位置取得
+  //どのデバイスになっても変わらない値が左上だから
+  //スクロール量 = Yの値 (window.pageYOffset) || スクロール縦にどれほどスクロールしたかを取得
+  //ブラウザによって効かないことがあるので、２つ用意してる
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   //画面TOPの座標を取得する
   let target = topheight.bottom + scrollTop;
@@ -57,15 +60,37 @@ window.addEventListener("mousemove", function (mouse) {
   // console.log(mY);
 });
 
-//お手本を分解して一個ずつ作っていく
-//作りたいもの = 要素が指定の箇所まで来たときcssを追加する
-//指定の箇所 = どうわからせる？
-//1.対象となる要素を取得してくる
-let section_02 = document.getElementById("section_02");
-//要素の寸法と位置を取得
-let size = section_02.getBoundingClientRect();
-//
+window.addEventListener("scroll", function () {
+  //お手本を分解して一個ずつ作っていく
+  //作りたいもの = 要素が指定の箇所まで来たときcssを追加する
+  //指定の箇所 = どうわからせる？
+  //1.対象となる要素を取得してくる
+  let section_02 = document.getElementById("section_02");
+  //要素の寸法と位置を取得
+  let size = section_02.getBoundingClientRect();
+  //スクロールした値を取得する
+  let Top = window.pageYOffset || document.documentElement.scrollTop;
+  //画面TOPの座標を取得する
+  let target2 = size.bottom + Top;
+  //画面の高さ
+  let WH = parseFloat(window.innerHeight);
+  // スクロール位置からウィンドウの高さ分を減算
+  let point2 = target2 - WH;
 
-//動いてるか確認
-// console.log(section_02);
-// console.log(size);
+  //動いてるか確認
+  // console.log(section_02);
+  // console.log(size);
+  // console.log(target2);
+  // console.log(WH);
+  // console.log(point2);
+
+  if (Top + 300 > target2 - WH) {
+    section_02.style.opacity = "1";
+    section_02.style.backgroundColor = "#ffbebe";
+    section_02.style.transform = "translate(0, 0)";
+  } else {
+    section_02.style.opacity = "0";
+    section_02.style.backgroundColor = "#b3b3b3";
+    section_02.style.transform = "translate(0, 45px)";
+  }
+});
